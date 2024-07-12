953b803919839e72ecdab1ee4b15874f27fec9a6
theinvincible1504 <114138186+theinvincible1504@users.noreply.github.com>
2024-07-12 11:01:31 +0000

    variables tutorial

---
+++ basics/variables.js
@@ -0,0 +1,23 @@
+const accountId= 153274
+let accountEmail = "piyush@google.com"
+var accountPassword = "12345"
+accountCity = "Amritsar"
+let accountState;  // no value assigned to variable, so it prints "undefined" in the console
+
+// accountId = 2  //assignment to constant variable not allowed
+
+accountEmail = "pa@google.com"
+accountPassword = "41790"
+accountCity = "Mumbai"
+
+
+
+console.log(accountId)
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
