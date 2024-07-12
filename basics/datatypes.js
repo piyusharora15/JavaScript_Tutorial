fc6d34fc028ea6574efce516379319b3e51f487e
theinvincible1504 <114138186+theinvincible1504@users.noreply.github.com>
2024-07-12 11:40:33 +0000

    datatypes tutorial

---
+++ basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict";    //treat all js code as newer version
+
+// alert(3+3)  // we are using nodejs, not browser
+console.log(3
+    +
+    3)     // code readability should be high
+
+    console.log("Piyush")
+
+    let name = "Piyush"
+    let age = 21
+    let isLoggedIn = true
+    let state;
+
+    // number => 2 to power 53
+    // bigInt
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined => no value is assigned to the variable
+    // symbol => unique
+
+
+    // object
+
+    console.log(typeof undefined)  //undefined
+    console.log(typeof null)       // object
+
