133b2e60a5fa08f98d625cc3cf665cb83ee690d0
piyusharora15 <114138186+piyusharora15@users.noreply.github.com>
2024-09-14 06:32:50 +0000

    completed

--- basics/variables.js
+++ basics/variables.js
@@ -4,13 +4,11 @@ var accountPassword = "12345"
 accountCity = "Amritsar"
 let accountState;  // no value assigned to variable, so it prints "undefined" in the console
 
-// accountId = 2  //assignment to constant variable not allowed
+ //accountId = 2  //assignment to constant variable not allowed
 
 accountEmail = "pa@google.com"
 accountPassword = "41790"
 accountCity = "Mumbai"
-
-
 
 console.log(accountId)
 
