a082d1bf8e42c3cd32686d6631d467bf63719fa7
piyusharora15 <114138186+piyusharora15@users.noreply.github.com>
2024-09-17 14:05:45 +0000

    update

--- basics/comparison.js
+++ basics/comparison.js
@@ -4,9 +4,9 @@
 // console.log(2 == 1);
 // console.log(2 != 1);
 
-
-// console.log("2" > 1);
-// console.log("02" > 1);
+ console.log("2" > 1);
+ console.log("02" > 1);
+// It is converted into numeric datatype so it gives output as true.
 
 console.log(null > 0);
 console.log(null == 0);
@@ -15,9 +15,11 @@ console.log(null >= 0);
 // The reason is that an equality check == and comparisons > < >= <= work differently. 
 // Comparisons convert null to a number, treating it as 0. That's why (3) null>=0 is true
 // and (1) null>0 is false.
+
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
+// In this case it gives output as false in all cases.
 
 // Avoid these kind of comparisons as these are the inconsistencies in the language
 // sometimes it converts into 0 and sometimes into NaN
